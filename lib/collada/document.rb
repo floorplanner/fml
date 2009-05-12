7b85e0e65bb06986971c4e637636ea35e3edaf1c
Dusan Maliarik <dusan.maliarik@gmail.com>
2009-05-13 00:48:33 +0200

    COLLADA document initialization

--- lib/collada/document.rb
+++ lib/collada/document.rb
@@ -1,5 +1,17 @@
 module Collada
   class Document < XML::Document
+    XMLNS = "http://www.collada.org/2005/11/COLLADASchema"
+    VERSION = "1.4.1"
+    def initialize
+      self.root = XML::Node.new('kml')
+      self.root.namespaces.namespace = XML::Namespace.new(self.root,nil,XMLNS)
+      self.root.attributes['version'] = VERSION
+    end
+
+    def << (node)
+      root << node
+    end
+
     def place(doc,position,rotation,scale)
       # merge in doc, and instantiate at visual scene
       # with given coordinates
