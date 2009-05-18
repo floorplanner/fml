5ac6d88aefced1097f85f8495e8be8147a36ba1a
Valery Vishnyakov <valery.visnakov@rails.lv>
2009-05-18 20:13:58 +0300

    Ugly namespacing

--- lib/collada/asset.rb
+++ lib/collada/asset.rb
@@ -16,20 +16,48 @@ module Collada
     end
 
     def library_materials
-      @xml.find(LIBRARY_MATERIALS)
+      materials = @xml.find(LIBRARY_MATERIALS)
+      materials.each {|mat| namespace!(mat)}
+      materials
     end
 
     def library_effects
-      @xml.find(LIBRARY_EFFECTS)
+      effects = @xml.find(LIBRARY_EFFECTS)
+      effects.each {|eff| namespace!(eff)}
+      effects
     end
 
     def library_geometries
-      @xml.find(LIBRARY_GEOMETRIES)
+      geometries = @xml.find(LIBRARY_GEOMETRIES)
+      geometries.each{|geo| namespace!(geo)}
+      geometries
     end
 
     def library_visual_scenes
-      @xml.find(VISUAL_SCENES_QUERY).first
+      namespace!(@xml.find(VISUAL_SCENES_QUERY).first)
     end
+
+    private
+      def namespace!(node)
+        node['id'] = "#{namespace_name}.#{node['id']}" if node['id']
+        node['name'] = "#{namespace_name}.#{node['name']}" if node['name']
+        node['symbol'] = "#{namespace_name}.#{node['symbol']}" if node['symbol']
+        node['material'] = "#{namespace_name}.#{node['material']}" if node['material']
+
+
+        node['url'] = "##{namespace_name}.#{node['url'].gsub('#','')}" if node['url']
+        node['target'] = "##{namespace_name}.#{node['target'].gsub('#','')}" if node['target']
+        node['source'] = "##{namespace_name}.#{node['source'].gsub('#','')}" if node['source']
+
+        node.children.each do |children|
+          namespace!(children)
+        end
+        node
+      end
+
+      def namespace_name
+        self.name.gsub('.','_')
+      end
 
   end
 end
