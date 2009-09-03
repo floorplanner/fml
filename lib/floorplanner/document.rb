df944ef396a9dcd3b430047ced7bcd8a78d03100
Dusan Maliarik <dusan.maliarik@gmail.com>
2009-09-03 16:55:25 +0200

    added support for updating wall heights

--- lib/floorplanner/document.rb
+++ lib/floorplanner/document.rb
@@ -1,4 +1,5 @@
 module Floorplanner
+
   class Document
     POINTS_QUERY = "/project/floors/floor/designs/design/area/line/points"
     LINE_POINTS_REGEXP = /^((\s*[-+]?[0-9]*\.?[0-9]+\s+){5,8}\s*[-+]?[0-9]*\.?[0-9]+\s*?(?:,)?)*$/
@@ -18,7 +19,7 @@ module Floorplanner
       end
     end
 
-    private
+  private
 
     def self.validate_line_points(doc)
       doc.find(POINTS_QUERY).each do |points_node|
@@ -29,4 +30,37 @@ module Floorplanner
       end
     end
   end
+
+  class DesignDocument
+    def initialize(fml_fn)
+      @xml = XML::Document.file(fml_fn)
+    end
+
+    def update_heights(new_height)
+      lines = @xml.find("/design/lines/line[type='default_wall' or type='normal_wall' or contains(type,'hedge') or contains(type,'fence')]")
+      lines.each do |line|
+        begin
+          points = line.find("points").first
+          next unless points.content.include? ","
+
+          coords = points.content.strip.split(",")
+          top_coords = coords[1].strip.split(/\s/).map{|c| c.to_f}
+
+          top_coords[2] = new_height
+          top_coords[5] = new_height
+          if top_coords.length > 6
+            top_coords[8] = new_height
+          end
+
+          coords[1] = top_coords.join(" ")
+          points.content = coords.join(",")
+        rescue; end
+      end
+    end
+
+    def save(fn)
+      @xml.save fn
+    end
+  end
+
 end
